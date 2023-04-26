4645abdec8ce18278dfced1a3d9191213c17010e
Bibek Angdembe <Bibekangdembay@gmail.com>
2023-04-26 16:09:50 +0545

    initial file setup completed

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import Appointment from "./components/appointment/Appointment";
+import Aboutus from "./components/aboutus/Aboutus";
+
+const Home = () => {
+  return (
+    <>
+        <Aboutus />
+        <Appointment />
+        <h1>Hello</h1>
+    </>
+  )
+}
+export default Home
---
+++ src/components/aboutus/Aboutus.jsx
@@ -0,0 +1,7 @@
+import './Aboutus.css'
+const Aboutus = () => {
+  return (
+    <div>Aboutus</div>
+  )
+}
+export default Aboutus
