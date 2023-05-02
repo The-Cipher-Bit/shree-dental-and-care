a871c0c936fe354f00045b17de8546fa90354c57
sakshamkadayat <sakshamkadayat@gmail.com>
2023-05-02 11:18:02 +0545

    section 2 section 3 completed

---
+++ src/components/MeetOurSpecialist/Meet.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Meet.css";
+function Meet() {
+  return (
+    <>
+    <div className="container-meet-specialist">
+        <h1>Meet</h1>
+        <h1 className='colo'>Our Specialist</h1>
+    </div>
+    </>
+  )
+}
+
+export default Meet
