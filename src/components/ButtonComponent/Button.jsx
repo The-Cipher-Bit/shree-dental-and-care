5f9d40e53ab36a3502fe422665762d58625acae8
Bibek Angdembe <Bibekangdembay@gmail.com>
2023-05-05 23:04:34 +0545

    consultation , framer ,scroll to top ,button all in progress

--- src/components/ButtonComponent/Button.jsx
+++ src/components/ButtonComponent/Button.jsx
@@ -1,5 +1,5 @@
 import './button.css'
-const Button = ({text,padLeft}) => {
-  return <button className="reusable__btn" style={{paddingLeft:`${padLeft}`,paddingRight:`${padLeft}`}}><a href="http://">{text}</a></button>;
+const Button = ({text,padLeft = '30px'}) => {
+  return <button className="reusable__btn" style={{paddingLeft:`${padLeft}`,paddingRight:`${padLeft}`}}><a href="#">{text}</a></button>;
 }
 export default Button
--- src/components/sectiontwo/Consultation.jsx
+++ src/components/sectiontwo/Consultation.jsx
@@ -1,61 +1,151 @@
-import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import "./Consultation.css";
+import { RiUserFollowFill, RiTimeFill } from "react-icons/ri";
 import image from "../../assets/images/hero-teeth.png";
 import imagea from "../../assets/images/image 48.png";
 import imageb from "../../assets/images/image 49.png";
 import imagec from "../../assets/images/image 50.png";
-
 import Doctor from "../../assets/images/doctor.png";
 import Button from "../ButtonComponent/Button";
 
-const Detail = [
+const details = [
   {
     id: 1,
-    className: "details-section-1",
-    details: "Leaser Technology",
+    doctorName: "Dr.Jhon Doe",
+    speciality: "Dental Surgeon",
+    field: "Leaser Technology",
     image: imagea,
-    detaila:'Revolutionizing dental care with laser technology.'
+    doctorImage: Doctor,
+    time: "10:00 AM - 3:00 PM",
+    detail: "Revolutionizing dental care with laser technology.",
   },
   {
     id: 2,
-    className: "details-section-2",
-    details: "Painless Technology",
+    doctorName: "Dr. Jiten karki",
+    speciality: "Dental Surgeon",
+    field: "Painless Technology",
     image: imageb,
+    doctorImage: Doctor,
+    time: "10:00 AM - 6:00 PM",
+    detail: "Revolutionizing dental care with laser technology.",
   },
   {
     id: 3,
-    className: "details-section-3",
-    details: "3D Dental Scanner",
+    doctorName: "Dr.Chandra",
+    speciality: "Dental Surgeon",
+    field: "3D Dental Scanner",
     image: imagec,
+    doctorImage: Doctor,
+    time: "10:00 AM - 3:00 PM",
+    detail: "Revolutionizing dental care with laser technology.",
   },
 ];
 function Consultation() {
+  const [activeDoctor, setActiveDoctor] = useState(details[0]);
+  const sectionVariants = {
+    initial: {
+      x: "-100%",
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    exit: {
+      x: "100%",
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
   return (
     <>
-      <div className="container-consultation">
-        <div className="container-consultation-left">
-         <div className="consultation-image-container">
-          <img src={Doctor} alt="Doctor" />
-        </div>
-        <div className="container-bookappointment">
-          <p>Dr.Jhon Doe</p>
-          <Button text="Book Appointment" padLeft="25px"/>
-        </div>
-        </div>
-       
-        <div className="container-consultation-right">
-          <h2 className="textcolor_secondary title_font" style={{fontSize:'2.8rem'}}>Find Your Right Doctor</h2>
-          <h1 className="textcolor_primary title_font" style={{fontSize:'2.8rem'}}>For Best Consultation</h1>
-          <div className="detail-section">
-            {Detail.map((details) => (
-              <div key={details.id} className={details.className}>
-                <img src={details.image} alt="details" />
-                <p>{details.details}</p>
+      <AnimatePresence>
+        <motion.div
+          key={activeDoctor.id}
+          variants={sectionVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          <div className="container-consultation">
+            <div className="container-consultation-left">
+              <div className="consultation-image-container">
+                <img src={Doctor} alt="Doctor" />
               </div>
-            ))}
+              <div className="container-bookappointment">
+                <div className="container-bookappointment_doctor_info">
+                  <div className="container-bookappointment_doctor_info_left">
+                    <RiUserFollowFill size={25} color="white" />
+                  </div>
+                  <div className="container-bookappointment_doctor_info_right">
+                    <h2 className="title_font">{activeDoctor.doctorName}</h2>
+                    <p>{activeDoctor.speciality}</p>
+                  </div>
+                </div>
+                <div className="container-bookappointment_doctor_availability">
+                  <div className="container-bookappointment_doctor_time">
+                    <RiTimeFill size={25} color="black" />
+                  </div>
+                  <div className="container-bookappointment_doctor_time">
+                    <p>
+                      Time :{" "}
+                      <span style={{ color: "red" }}>{activeDoctor.time}</span>
+                    </p>
+                  </div>
+                </div>
+                <Button text="Book Appointment" padLeft="20px" />
+              </div>
+            </div>
+
+            <div className="container-consultation-right">
+              <h2
+                className="textcolor_secondary title_font"
+                style={{ fontSize: "2.8rem" }}
+              >
+                Find Your Right Doctor
+              </h2>
+              <h1
+                className="textcolor_primary title_font"
+                style={{ fontSize: "2.8rem", marginBottom: "1rem" }}
+              >
+                For Best Consultation
+              </h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
+                optio ullam ab eveniet iusto unde vero error ea numquam,
+                eligendi natus.
+              </p>
+              <div className="doctors_field_container">
+                {details.map((detail) => (
+                  <div
+                    key={detail.id}
+                    onClick={() => setActiveDoctor(detail)}
+                    className={`doctors_field_card ${
+                      activeDoctor.id === detail.id
+                        ? "doctors_field_card_active"
+                        : ""
+                    }`}
+                  >
+                    <div className="detail-section-field-image">
+                      <img src={detail.image} alt="details" />
+                    </div>
+                    <div className="detail-section-field-data">
+                      <h2>{detail.field}</h2>
+                      <p>{detail.detail}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
