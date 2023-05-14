ba14125fdcb9011df905052543270ac9a0a81f4c
Bibek Angdembe <Bibekangdembay@gmail.com>
2023-05-14 07:10:59 +0545

    responseive navbar and sllider 80% done with framer animation for dropdown

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,9 +4,79 @@ import logo from "../../assets/images/templogo.png";
 import Heroslider from "./HeroSlider";
 import Teeth from "../../assets/images/hero-teeth.png";
 import Button from "../ButtonComponent/Button";
+import { RiMenuFill, RiCloseFill } from "react-icons/ri";
+import { AnimatePresence , motion } from "framer-motion";
+
+const NavLinks = ({ handleLink = ''}) =>{
+  return (
+    <ul className="nav-list">
+      <li>
+        <a href="#" onClick={handleLink}>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#about" onClick={handleLink}>
+          About Us
+        </a>
+      </li>
+      <li>
+        <a href="#consult" onClick={handleLink}>
+          Consultation
+        </a>
+      </li>
+      <li>
+        <a href="#contact" onClick={handleLink}>
+          Contact Us
+        </a>
+      </li>
+      <li>
+        <a href="#gallery" onClick={handleLink}>
+          Gallery
+        </a>
+      </li>
+    </ul>
+  );
+}
 
 const HeroSection = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  //framer motion variants
+  const navVariants = {
+    hidden: {
+      opacity: 0,
+      transform: "translateY(-100%)",
+    },
+    visible: {
+      opacity: 1,
+      transform: "translateY(0%)",
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        duration: 0.5,
+        delay: 0.3,
+      },
+    },
+    leave: {
+      opacity: 0,
+      transform: "translateY(-100%)",
+      transition: {
+        type: "spring",
+        duration: 1,
+        stiffness: 100,
+        delay: 0.3,
+      },
+    },
+  };
+
+
+  const handleToggle = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY > 0 ? setScrolled(true) : setScrolled(false);
@@ -22,55 +92,59 @@ const HeroSection = () => {
   // };
   // window.addEventListener("scroll", handleScroll);
 
-  const linkColor = scrolled ? { color: "black" } : {};
   return (
-    <header>
-      <nav className={scrolled ? "scrolled" : ""}>
-        <div className="logo">
-          <img src={logo} alt="Shree Dental Logo" />
+    <>
+      {toggleMenu && (
+        <div className="navlinks_container">
+          <NavLinks />
         </div>
-        <ul className="nav-list">
-          <li>
-            <a style={linkColor} href="#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a style={linkColor} href="#about">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a style={linkColor} href="#consult">
-              Consultation
-            </a>
-          </li>
-          <li>
-            <a style={linkColor} href="#contact">
-              Contact Us
-            </a>
-          </li>
-          <li>
-            <a style={linkColor} href="#gallery">
-              Gallery
-            </a>
-          </li>
-        </ul>
-        <Button text="Plan Your Visit" url="#appointment" padLeft="25px" />
-      </nav>
-      <Heroslider />
-      <div className="container-teeth">
-        <div className="container-teeth-image">
-          <img src={Teeth} alt="teeth" />
+      )}
+      <header>
+        <nav className={`${scrolled ? "scrolled" : ""}`}>
+          <div className="logo">
+            <img src={logo} alt="Shree Dental Logo" />
+          </div>
+          <div className="navlinks_container">
+            <NavLinks />
+          </div>
+          <div className="clickable_buttons">
+            <Button text="Plan Your Visit" url="#appointment" padLeft="25px" />
+            {toggleMenu ? (
+              <RiCloseFill onClick={handleToggle} className="menu-close-icon" />
+            ) : (
+              <RiMenuFill onClick={handleToggle} className="menu-open-icon" />
+            )}
+          </div>
+        </nav>
+        <Heroslider />
+        <div className="container-teeth">
+          <div className="container-teeth-image">
+            <img src={Teeth} alt="teeth" />
+          </div>
+          <div className="container-teeth-content">
+            <p>
+              We can restore{" "}
+              <span className="teeth-text-color">your dental health issue</span>
+            </p>
+          </div>
         </div>
-        <div className="container-teeth-content">
-          <p>
-            We can restore{" "}
-            <span className="teeth-text-color">your dental health issue</span>
-          </p>
-        </div>
-      </div>
-    </header>
+      </header>
+      <AnimatePresence>
+        {toggleMenu && (
+          <motion.div
+            key={1}
+            initial="hidden"
+            animate="visible"
+            exit="leave"
+            variants={navVariants}
+            className="navlinks_container_mobile_tabs"
+          >
+            <NavLinks handleLink = {handleToggle} />
+            <RiCloseFill onClick={handleToggle} className="menu-close-icon" />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 export default HeroSection;
